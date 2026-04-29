6cad2cb61b82a933fb3ae25aa479b09cb78e0fe9
pereiramemo <pereiramemo@gmail.com>
2026-04-29 13:43:48 +0000

    removed moedules 3 and 4 and merged into module3-4

--- docker/dockerbuild_commands.sh
+++ docker/dockerbuild_commands.sh
@@ -2,5 +2,7 @@ SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
 docker build --network=host -f ${SCRIPT_DIR}/Dockerfile.module-1 -t ghcr.io/epereira/mg-clust/module-1:latest .
 docker build --network=host -f ${SCRIPT_DIR}/Dockerfile.module-2 -t ghcr.io/epereira/mg-clust/module-2:latest .
-docker build --network=host -f ${SCRIPT_DIR}/Dockerfile.module-3 -t ghcr.io/epereira/mg-clust/module-3:latest .
-docker build --network=host -f ${SCRIPT_DIR}/Dockerfile.module-4 -t ghcr.io/epereira/mg-clust/module-4:latest .
+docker build --network=host -f ${SCRIPT_DIR}/Dockerfile.module-3-4 -t ghcr.io/epereira/mg-clust/module-3-4:latest .
+docker build --network=host -f ${SCRIPT_DIR}/Dockerfile.module-5 -t ghcr.io/epereira/mg-clust/module-5:latest .
+docker build --network=host -f ${SCRIPT_DIR}/Dockerfile.module-6 -t ghcr.io/epereira/mg-clust/module-6:latest .
+docker build --network=host -f ${SCRIPT_DIR}/Dockerfile.module-7 -t ghcr.io/epereira/mg-clust/module-7:latest .
